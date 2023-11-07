edd09cb25a79fae9d6c9f1fcc2cedff5e708df9c
adliph <bck434@gmail.com>
2023-11-07 20:49:36 +0300

    add views and urls

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Main page',
+                        'Your advertisement could be here...')
+
+def group_posts(request, group_name):
+    return HttpResponse(f'This group was made by: {group_name}')
 # Create your views here.
